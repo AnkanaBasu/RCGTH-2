6910270611407d858dfabb2e7bb1c1a68f6bbe58
Avik1998Sarkar <36466256+Avik1998Sarkar@users.noreply.github.com>
2021-02-04 14:08:34 +0530

    Add files via upload

---
+++ RCGTH-IdentifyProfiles/src/main/java/com/cognizant/identifyprofiles/clients/ProfileClient.java
@@ -0,0 +1,16 @@
+package com.cognizant.identifyprofiles.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cognizant.identifyprofiles.model.Profile;
+
+@FeignClient(name = "RCGTH-Profile-Microservice", url = "http://35.188.208.181:8083")
+public interface ProfileClient {
+
+	@GetMapping("/profile/")
+	public List<Profile> getAllProfiles();
+
+}
